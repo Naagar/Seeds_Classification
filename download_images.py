2f7a3c05245ba40036ae3ad9d4c8a3a3e6523da6
Sandeep Nagar <rnfrbdsandeep@gmail.com>
2020-09-01 20:06:36 +0530

    Add files via upload

--- download_images.py
+++ download_images.py
@@ -1,17 +1,38 @@
-import panda as pd
+import pandas as pd
 import urllib.request
+import numpy as np
 
+def url_to_jpg(i, url, file_path_for_images, name):
 
-def url_to_jpg(i, urls):
-	
+	'''
+		args:
+			-- i number of image 
+			-- url : a URL address of a given image.
+			--file_path : where to save the final image.
+	'''
+
+	file_name = name
+	full_path = '{}{}'.format(file_path_for_images, file_name)
+	urllib.request.urlretrieve(url, full_path)
+
+	print('{} saved.'.format(file_name))
+
 	return None
 
 
-file_name = 'data_file.cvs'
+file_name = 'data_file.csv'
+file_path_for_images = 'seed_image/'
+urls = pd.read_csv(file_name)
 
-file_path_for_images = 'images/'
 
-urls = pd.read_cvs(file_name)
+for i, name  in enumerate(urls.values):
+	url ='https://s3.ap-south-1.amazonaws.com/adtech.monsanto/'
+	# print(name)
+	url = url + name
+	print(url[0])
 
-for i, urls in enumrate(urls.values):
+	# url = url.np.decode('utf-8')
+	# print(url)
+
+	url_to_jpg(i, url[0], file_path_for_images, name[0])
 
